0cfd6145c1b6181ff9b1dd0f50496364cde13c59
Dundych <dmitry.dundich@gmail.com>
2014-10-10 16:34:07 +0300

    leson1 variables

--- test.py
+++ test.py
@@ -5,3 +5,11 @@
 # centimeters = 100            one meter is 100 centimeters
 # nanosecond = 1.0/1000000000  one billionth of a second
 print 299792458 *100 / 1000000000
+
+# Given the variables defined here, write Python 
+# code that prints out the distance, in meters, 
+# that light travels in one processor cycle. 
+
+speed_of_light = 299792458.0
+cycles_per_second = 2700000000.0
+print speed_of_light / cycles_per_second
