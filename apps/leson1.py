c03edf2f2e57bfca142bdb2b736617f3437fa0fc
Dmytro <dmitry.dundich@gmail.com>
2014-10-11 11:03:56 +0300

    Home work

--- apps/leson1.py
+++ apps/leson1.py
@@ -72,3 +72,77 @@ nanodistance = speed_of_light / nano_per_sec
 
 print nanodistance
 
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+print s[:2] + t[3:]
+
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the first occurrence of 'hoo'
+# in the value of text, or -1 if
+# it does not occur at all.
+
+text = "first hoo" 
+
+# ENTER CODE BELOW HERE
+
+print text.find('hoo')
+
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the second occurrence of 'zip'
+# in text, or -1 if it does not occur
+# at least twice.
+
+# For example,
+#text = 'all zip files are zipped' 
+# >>> 18
+# text = 'all zip files are compressed'
+# >>> -1
+
+text = "all zip files are zipped" 
+
+#ENTER CODE BELOW HERE
+print text.find( 'zip' , text.find('zip') + 1 )
+
+# Given a variable, x, that stores the 
+# value of any decimal number, write Python 
+# code that prints out the nearest whole 
+# number to x.
+# If x is exactly half way between two 
+# whole numbers, round up, so
+# 3.5 rounds to 4 and 2.5 rounds to 3.
+# You may assume x is not negative.
+
+# Hint: The str function can convert any number into a string.
+# eg str(89) converts the number 89 to the string '89'
+
+# Along with the str function, this problem can be solved 
+# using just the information introduced in unit 1.
+
+# x = 3.14159 
+# >>> 3 (not 3.0)
+# x = 27.63 
+# >>> 28 (not 28.0)
+# x = 3.5 
+# >>> 4 (not 4.0)
+
+x = 3.14159
+
+#ENTER CODE BELOW HERE
+after_point =  str(x)[ str(x).find(".") + 1 : str(x).find(".") + 2 ]
+upper = after_point.find('5') + 1
+upper = upper + after_point.find('6') + 1
+upper = upper + after_point.find('7') + 1
+upper = upper + after_point.find('8') + 1
+upper = upper + after_point.find('9') + 1
+x = x + upper
+x = str(x)[: str(x).find(".") ]
+print x
