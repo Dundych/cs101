6e960b5efaee62b4b5044df24dd1d82923a6a288
Dmytro <dmitry.dundich@gmail.com>
2014-10-11 15:56:59 +0300

    start lesson 2 proces

--- apps/leson2.py
+++ apps/leson2.py
@@ -21,3 +21,71 @@ def square(num):
 
 print square(5)
 #>>> 25
+
+#----------------------2---------------------------
+# Define a procedure, sum3, that takes three
+# inputs, and returns the sum of the three
+# input numbers.
+
+
+def sum3(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+
+
+
+
+print sum3(1,2,3)
+#>>> 6
+
+print sum3(93,53,70)
+#>>> 216
+
+
+#----------------------3-----------------------
+# Define a procedure, abbaize, that takes
+# two strings as its inputs, and returns
+# a string that is the first input,
+# followed by two repetitions of the second input,
+# followed by the first input.
+
+
+def abbaize(s1, s2):
+    return s1+s2*2+s1
+
+
+
+
+
+
+print abbaize('a','b')
+#>>> 'abba'
+
+print abbaize('dog','cat')
+#>>> 'dogcatcatdog'
+
+#------------------------4-----------------------------
+
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
+
+
+def find_second(s1, s2):
+    return s1.find(s2, s1.find(s2) + 1)
+
+
+
+
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
