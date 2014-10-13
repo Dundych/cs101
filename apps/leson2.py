a7193148e73705d17b967c6121e96f71e4f740f2
Dmytro <dmitry.dundich@gmail.com>
2014-10-14 00:16:25 +0300

    hw

--- apps/leson2.py
+++ apps/leson2.py
@@ -245,4 +245,39 @@ def get_next_target(page):
     return url, end_quote
                  
 
+#----------------------------Home Work-----------------------
+# Define a procedure, udacify, that takes as
+# input a string, and returns a string that
+# is anhttps://www.udacity.com/course/viewer uppercase 'U' followed by the input string.
+# for example, when you enter
+
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+
+# Make sure your procedure has a return statement.
+
+def udacify(s):
+    return 'U' + s
+
+
+
+
+
+
+# Remove the hash, #, from infront of print to test your code.
+
+print udacify('dacians')
+#>>> Udacians
+
+print udacify('turn')
+#>>> Uturn
+
+print udacify('boat')
+#>>> Uboat
+
+
+
+
+
 
