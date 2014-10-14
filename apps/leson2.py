d1a926a6be414a318c4f9394c9fb2a6ec2d4cc9b
Dundych <dmitry.dundich@gmail.com>
2014-10-14 14:34:11 +0300

    HWpart2process

--- apps/leson2.py
+++ apps/leson2.py
@@ -479,6 +479,127 @@ print set_range(10, 4, 7)
 print set_range(1.1, 7.4, 18.7)
 #>>> 17.6 # since 18.7 - 1.1 = 17.6
 
+#--------------------------------8--------------------------
+
+# By Sam the Great from forums
+# That freaking superhero has been frequenting Udacity
+# as his favorite boss battle fight stage. The 'Udacity'
+# banner keeps breaking, and money is being wasted on
+# repairs. This time, we need you to proceduralize the
+# fixing process by building a machine to automatically
+# search through debris and return the 'Udacity' banner
+# to the company, and be able to similarly fix other goods.
+
+# Write a Python procedure fix_machine to take 2 string inputs
+# and returns the 2nd input string as the output if all of its
+# characters can be found in the 1st input string and "Give me
+# something that's not useless next time." if it's impossible.
+# Letters that are present in the 1st input string may be used
+# as many times as necessary to create the 2nd string (you
+# don't need to keep track of repeat usage).
+
+# NOTE: # If you are experiencing difficulties taking
+        # this problem seriously, please refer back to
+        # "Superhero flyby", the prequel, in Problem Set 11.
+
+# TOOLS: # if statement
+         # while loop
+         # string operations
+         # Unit 1 Basics
+
+# BONUS: # 
+# 5***** #  If you've graduated from CS101,
+#  Gold  #  try solving this in one line.
+# Stars! #
+
+def fix_machine(debris, product):
+    ### WRITE YOUR CODE HERE ###
+    pos = 0
+    while pos < len(product):
+        if debris.find(product[pos]) == -1:
+           return  "Give me something that's not useless next time."
+        pos = pos +1
+    return product
+
+### TEST CASES ###
+print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me something that's not useless next time."
+print "Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity'
+print "Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity'
+print "Test case 4: ", fix_machine('wsx0-=mttrhix', 't-shirt') == 't-shirt'
+
+#---------------------------9------------------------
+
+# By Websten from forums
+#
+# Given your birthday and the current date, calculate your age in days. 
+# Account for leap days. 
+#
+# Assume that the birthday and current date are correct dates (and no 
+# time travel). 
+#
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    ##
+    # Your code here.
+    ##
+    def isLeapYear(year):
+        if (year % 4) != 0:
+            return 0
+        if (year % 100) != 0: 
+            return 1
+        if (year % 400) != 0:
+            return 0
+        return 1
+    
+    def dayOnMonth(m):
+        if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m ==12:
+            return 31
+        if m == 4 or m == 6 or m == 9 or m == 11:
+            return 30
+        if m == 2:            
+            return 28
+    def dayFromStartYear(year, month, day) :
+        result = 0
+        countMonth = 1
+        while countMonth < month:
+            if countMonth == 2:
+                result = result + isLeapYear(year)
+            result = result + dayOnMonth(countMonth)
+            countMonth = countMonth + 1
+        return result + day
+    
+    result = 0
+    year = year1
+    while year2 - year > 1:
+        if isLeapYear(year):
+            result = result + 1
+        result = result + 365
+        year =  year + 1
+    if year2 - year1 > 0:
+        result = result + 365 + isLeapYear(year1) - dayFromStartYear(year1, month1, day1)
+        result = result + dayFromStartYear(year2, month2, day2)
+    else:
+        result = dayFromStartYear(year2, month2, day2) - dayFromStartYear(year1, month1, day1)
+    return result
+# Test routine 
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
+print daysBetweenDates(2011,6,30,2012,6,30)
+print daysBetweenDates(2012,1,1,2012,2,28)
+
+
 
 
 
