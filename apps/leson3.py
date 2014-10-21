44cd4ed9a931aa261cff83009b2466a163611b89
Dundych <dmitry.dundich@gmail.com>
2014-10-21 19:17:27 +0300

    lesson3 to Loops p\on Lists

--- apps/leson3.py
+++ apps/leson3.py
@@ -16,3 +16,32 @@ print how_many_days(1)
 
 print how_many_days(9)
 #>>> 30
+
+#---------------------------------------2-----------------
+# Define a procedure, replace_spy,
+# that takes as its input a list of
+# three numbers, and modifies the
+# value of the third element in the
+# input list to be one more than its
+# previous value.
+
+spy = [0,0,7]
+def replace_spy(array):
+    array[len(array)-1] += 1
+
+
+
+
+
+
+
+
+
+# In the test below, the first line calls your 
+# procedure which will change spy, and the 
+# second checks you have changed it.
+# Uncomment the top two lines below.
+
+replace_spy(spy)
+print spy
+#>>> [0,0,8]
