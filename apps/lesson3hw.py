4d7a3ec37b7292cc9cec7e0642dc71e9d2c44d1b
Dundych <dmitry.dundich@gmail.com>
2014-10-23 18:54:14 +0300

    lesson 3 home work

--- apps/lesson3hw.py
+++ apps/lesson3hw.py
@@ -20,3 +20,177 @@ print product_list([1,2,3,4])
 
 print product_list([])
 #>>> 1
+
+#----------2----------------
+
+# Define a procedure, greatest,
+# that takes as input a list
+# of positive numbers, and
+# returns the greatest number
+# in that list. If the input
+# list is empty, the output
+# should be 0.
+
+def greatest(a):
+    res = 0
+    for el in a:
+        if res < el:
+            res = el
+    return res
+
+
+
+
+print greatest([4,23,1])
+#>>> 23
+print greatest([])
+#>>> 0
+#------3-------
+# Define a procedure, total_enrollment,
+# that takes as an input a list of elements,
+# where each element is a list containing
+# three elements: a university name,
+# the total number of students enrolled,
+# and the annual tuition fees.
+
+# The procedure should return two numbers,
+# not a string, 
+# giving the total number of students
+# enrolled at all of the universities
+# in the list, and the total tuition fees
+# (which is the sum of the number
+# of students enrolled times the
+# tuition fees for each university).
+
+udacious_univs = [['Udacity',90000,0]]
+
+usa_univs = [ ['California Institute of Technology',2175,37704],
+              ['Harvard',19627,39849],
+              ['Massachusetts Institute of Technology',10566,40732],
+              ['Princeton',7802,37000],
+              ['Rice',5879,35551],
+              ['Stanford',19535,40569],
+              ['Yale',11701,40500]  ]
+
+def total_enrollment(a):
+    st = 0
+    fees = 0
+    for el in a:
+       st += el[1]
+       fees += (el[1] * el[2])
+    return st, fees
+    
+
+print total_enrollment(udacious_univs)
+#>>> (90000,0)
+
+# The L is automatically added by Python to indicate a long
+# number. If you are trying the question in an outside 
+# interpreter you might not see it.
+
+print total_enrollment(usa_univs)
+#>>> (77285,3058581079)
+
+#------------4------------------
+# The web crawler we built at the end of Unit 3 has some serious
+# flaws if we were going to use it in a real crawler. One
+# problem is if we start with a good seed page, it might
+# run for an extremely long time (even forever, since the
+# number of URLS on the web is not actually finite). This
+# question and the following one explore two different ways
+# to limit the pages that it can crawl.
+
+# Modify the crawl_web procedure to take a second parameter,
+# max_pages, that limits the number of pages to crawl.
+# Your procedure should terminate the crawl after
+# max_pages different pages have been crawled, or when
+# there are no more pages to crawl.
+
+# The following definition of get_page provides an interface
+# to the website found at http://www.udacity.com/cs101x/index.html
+
+# The function output order does not affect grading.
+
+def get_page(url):
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return ('<html> <body> This is a test page for learning to crawl! '
+            '<p> It is a good idea to '
+            '<a href="http://www.udacity.com/cs101x/crawling.html">learn to '
+            'crawl</a> before you try to  '
+            '<a href="http://www.udacity.com/cs101x/walking.html">walk</a> '
+            'or  <a href="http://www.udacity.com/cs101x/flying.html">fly</a>. '
+            '</p> </body> </html> ')
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return ('<html> <body> I have not learned to crawl yet, but I '
+            'am quite good at '
+            '<a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.'
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return ('<html> <body> I cant get enough '
+            '<a href="http://www.udacity.com/cs101x/index.html">crawling</a>! '
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return ('<html> <body> The magic words are Squeamish Ossifrage! '
+            '</body> </html>')
+    except:
+        return ""
+    return ""
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed, max_pages):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+            if len(crawled) >= max_pages:
+                break
+    return crawled
+
+print crawl_web("http://www.udacity.com/cs101x/index.html",1) 
+#>>> ['http://www.udacity.com/cs101x/index.html']
+
+print crawl_web("http://www.udacity.com/cs101x/index.html",3) 
+#>>> ['http://www.udacity.com/cs101x/index.html', 
+#>>> 'http://www.udacity.com/cs101x/flying.html', 
+#>>> 'http://www.udacity.com/cs101x/walking.html']
+
+#print crawl_web("http://www.udacity.com/cs101x/index.html",500) 
+#>>> ['http://www.udacity.com/cs101x/index.html', 
+#>>> 'http://www.udacity.com/cs101x/flying.html', 
+#>>> 'http://www.udacity.com/cs101x/walking.html', 
+#>>> 'http://www.udacity.com/cs101x/crawling.html', 
+#>>> 'http://www.udacity.com/cs101x/kicking.html']
+
+    
+
+
+    
+
