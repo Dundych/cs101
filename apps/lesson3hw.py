2abaa3c41c7f4e201831e50febaae7e328c7b7ab
Dundych <dmitry.dundich@gmail.com>
2014-10-24 12:08:59 +0300

    lesson 3 problem set done

--- apps/lesson3hw.py
+++ apps/lesson3hw.py
@@ -343,6 +343,129 @@ print crawl_web("A1",3)
 #>>> ['A1', 'C1', 'B1', 'E1', 'D1', 'F1']
 # (May be in any order)
 
+#----6-----
+
+# THREE GOLD STARS
+
+# Sudoku [http://en.wikipedia.org/wiki/Sudoku]
+# is a logic puzzle where a game
+# is defined by a partially filled
+# 9 x 9 square of digits where each square
+# contains one of the digits 1,2,3,4,5,6,7,8,9.
+# For this question we will generalize
+# and simplify the game.
+
+# Define a procedure, check_sudoku,
+# that takes as input a square list
+# of lists representing an n x n
+# sudoku puzzle solution and returns the boolean
+# True if the input is a valid
+# sudoku square and returns the boolean False
+# otherwise.
+
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+
+def is_corect(some_list):
+    list_valids_value =[]
+    n = len(some_list)
+    while n:
+       list_valids_value.append(n)
+       n -= 1
+    unic = []
+    while some_list:
+        el = some_list.pop()
+        if (el in unic) or (el not in list_valids_value):
+           return False
+        unic.append(el)
+    return True
+
+
+def is_valid(list_of_lists):
+    for OneList in list_of_lists:
+        if not is_corect(OneList):
+           return False
+    return True
+
+def sort_by_columns(list_of_lists):
+    res = []
+    n = len(list_of_lists)
+    while n:
+        row = []
+        for OneList in list_of_lists:
+            row.append(OneList[-n])
+        res.append(row)
+        n -= 1
+    return res
+        
+
+def check_sudoku(square):
+    for el in square:
+        if len(el) != len(square):
+            return False
+    list_rows = square
+    list_columns = sort_by_columns(square)
+    if is_valid(list_rows) and is_valid(list_columns):
+        return True
+    return False
+
+
+print check_sudoku(incorrect)
+#>>> False
+
+print check_sudoku(correct)
+#>>> True
+
+print check_sudoku(incorrect2)
+#>>> False
+
+print check_sudoku(incorrect3)
+#>>> False
+
+print check_sudoku(incorrect4)
+#>>> False
+
+print check_sudoku(incorrect5)
+#>>> False
+
+
+
     
 
 
