2e768e14a334510e8b6f6b15037cfd715726d2ad
Dundych <dmitry.dundich@gmail.com>
2014-10-30 12:54:24 +0200

    Lesson 4 half passed

--- apps/lesson4.py
+++ apps/lesson4.py
@@ -35,10 +35,71 @@ if __name__ == '__main__':
 
 #add_to_index(index,'udacity','http://udacity.com')
 #add_to_index(index,'computing','http://acm.org')
-#add_to_index(index,'udacity','http://npr.org')
+#add_to_index(index,'udacity','http://npr.or
 print index
 #>>> [['udacity', ['http://udacity.com', 'http://npr.org']], 
 #>>> ['computing', ['http://acm.org']]]
+
+#---2---
+# Define a procedure, lookup,
+# that takes two inputs:
+
+# - an index
+# - keyword
+
+# The procedure should return a list
+# of the urls associated
+# with the keyword. If the keyword
+# is not in the index, the procedure
+# should return an empty list.
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+def lookup(index,keyword):
+    for el in index:
+        if el[0] == keyword:
+            return el[1]
+    return []
+
+
+print lookup(index,'udacity')
+#>>> ['http://udacity.com','http://npr.org']
+
+#---3---
+# Define a procedure, add_page_to_index,
+# that takes three inputs:
+
+#   - index
+#   - url (String)
+#   - content (String)
+
+# It should update the index to include
+# all of the word occurences found in the
+# page content by adding the url to the
+# word's associated url list.
+
+index = []
+
+
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+
+def add_page_to_index(index,url,content):
+    page = content.split()
+    for word in page:
+        add_to_index(index,word,url)
+
+add_page_to_index(index,'fake.text',"This is a test")
+print index
+#>>> [['This', ['fake.text']], ['is', ['fake.text']], ['a', ['fake.text']],
+#>>> ['test',['fake.text']]]
+
+
 
 
 
