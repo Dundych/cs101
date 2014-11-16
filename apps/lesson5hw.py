0af2a1cb5047bd07238cd12a42ee28a7e8ed3231
Dundych <dmitry.dundich@gmail.com>
2014-11-16 13:25:11 +0200

    lesson 5 hw done

--- apps/lesson5hw.py
+++ apps/lesson5hw.py
@@ -245,7 +245,141 @@ hashtable_update(table, 'Python', 'Guido van Rossum')
 print hashtable_lookup(table, 'Python')
 #>>> Guido van Rossum
 
+#---3---
+# [Double Gold Star] Memoization is a way to make code run faster by saving
+# previously computed results.  Instead of needing to recompute the value of an
+# expression, a memoized computation first looks for the value in a cache of
+# pre-computed values.
+
+# Define a procedure, cached_execution(cache, proc, proc_input), that takes in
+# three inputs: a cache, which is a Dictionary that maps inputs to proc to
+# their previously computed values, a procedure, proc, which can be called by
+# just writing proc(proc_input), and proc_input which is the input to proc.
+# Your procedure should return the value of the proc with input proc_input,
+# but should only evaluate it if it has not been previously called.
+def call_func(name, inp):
+    return name(inp)
+
+def cached_execution(cache, proc, proc_input):
+    # Your code here
+    # cache = {'proc':{'key':val}}
+    key = str(proc)[str(proc).find('function') + 9:]
+    key = key[:key.find(' ')]
+    print key
+    if key in cache:
+       if str(proc_input) in cache[key]:
+            return cache[key][str(proc_input)]
+    else:
+        cache[key] = {}
+    cache[key][str(proc_input)] = proc(proc_input)
+    return cache[key][str(proc_input)]
+
+# Here is an example showing the desired behavior of cached_execution:
+
+def factorial(n):
+    print "Running factorial"
+    result = 1
+    for i in range(2, n + 1):
+        result = result * i
+    return result
+
+cache = {} # start cache as an empty dictionary
+### first execution (should print out Running factorial and the result)
+print cached_execution(cache, factorial, 50)
+
+print "Second time:"
+### second execution (should only print out the result)
+print cached_execution(cache, factorial, 50)
+
+# Here is a more interesting example using cached_execution
+# (do not worry if you do not understand this, though,
+# it will be clearer after Unit 6):
+
+def cached_fibo(n):
+    if n == 1 or n == 0:
+        return n
+    else:
+        return (cached_execution(cache, cached_fibo, n - 1 )
+               + cached_execution(cache,  cached_fibo, n - 2 ))
+               
+cache = {} # new cache for this procedure
+# do not try this at home...at least without a cache!
+print cached_execution(cache, cached_fibo,100)
+#----4----
+# Write a procedure, shift, which takes as its input a lowercase letter,
+# a-z and returns the next letter in the alphabet after it, with 'a' 
+# following 'z'.
+
+def shift(letter):
+    num = ord(letter)
+    if num == ord('z'):
+       num = ord('a')-1 
+    return chr(num + 1)
 
 
+print shift('a')
+#>>> b
+print shift('n')
+#>>> o
+print shift('z')
+#>>> a
+#-----5-----# Write a procedure, shift_n_letters which takes as its input a lowercase
+# letter, a-z, and an integer n, and returns the letter n steps in the
+# alphabet after it. Note that 'a' follows 'z', and that n can be positive,
+#negative or zero.
+
+def shift_n_letters(letter, n):
+    num = ord(letter)- ord('a')
+    alphabet = []
+    for i in xrange(26):
+            alphabet.append(chr(ord('a')+i))
+    return alphabet[(n+num)%len(alphabet)]
+    
+
+
+print shift_n_letters('s', 1)
+#>>> t
+print shift_n_letters('s', 2)
+#>>> u
+print shift_n_letters('s', 10)
+#>>> c
+print shift_n_letters('s', -10)
+#>>> i
+#---6---
+# Write a procedure, rotate which takes as its input a string of lower case
+# letters, a-z, and spaces, and an integer n, and returns the string constructed
+# by shifting each of the letters n steps, and leaving the spaces unchanged.
+# Note that 'a' follows 'z'. You can use an additional procedure if you
+# choose to as long as rotate returns the correct string.
+# Note that n can be positive, negative or zero.
+
+def shift_n_letters(letter, n):
+    num = ord(letter)- ord('a')
+    alphabet = []
+    for i in xrange(26):
+            alphabet.append(chr(ord('a')+i))
+    return alphabet[(n+num)%len(alphabet)]
+
+def rotate(string, n):
+    # Your code here
+    result = ''
+    for i in xrange(len(string)):
+        if string[i] != ' ':
+            result+= shift_n_letters(string[i],n)
+        else:
+            result+= string[i]
+    return result    
+
+print rotate ('sarah', 13)
+#>>> 'fnenu'
+print rotate('fnenu',13)
+#>>> 'sarah'
+print rotate('dave',5)
+#>>>'ifaj'
+print rotate('ifaj',-5)
+#>>>'dave'
+print rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
+                "sv rscv kf ivru kyzj"),-17)
+#>>> ???
 
 
