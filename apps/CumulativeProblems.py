e065953e36d564a1f29e9f3f3197ebae3f32ad64
Dundych <dmitry.dundich@gmail.com>
2014-11-25 12:34:16 +0200

    cumulative Problems

--- apps/CumulativeProblems.py
+++ apps/CumulativeProblems.py
@@ -59,3 +59,42 @@ print triangular(3)
 
 print triangular(10)
 #>>> 55
+
+# Question 4: Remove Tags
+
+# When we add our words to the index, we don't really want to include
+# html tags such as <body>, <head>, <table>, <a href="..."> and so on.
+
+# Write a procedure, remove_tags, that takes as input a string and returns
+# a list of words, in order, with the tags removed. Tags are defined to be
+# strings surrounded by < >. Words are separated by whitespace or tags. 
+# You may assume the input does not include any unclosed tags, that is,  
+# there will be no '<' without a following '>'.
+
+def remove_tags(s):
+    while s.find('<') != -1:
+       s = remove_first_tag(s)
+    return s.split()
+
+def remove_first_tag(s):
+    tag = s[s.find("<"):s.find(">")+1]
+    s = s[:s.find(tag)] +' '+ s[s.find(tag)+len(tag):]
+    return s
+
+print remove_first_tag('''Title</h1><p>This is a
+                    <a href="http://www.udacity.com">link</a>.<p>''')
+
+print remove_tags('''<h1>Title</h1><p>This is a
+                    <a href="http://www.udacity.com">link</a>.<p>''')
+#>>> ['Title','This','is','a','link','.']
+
+print remove_tags('''<table cellpadding='3'>
+                     <tr><td>Hello</td><td>World!</td></tr>
+                     </table>''')
+#>>> ['Hello','World!']
+
+print remove_tags("<hello><goodbye>")
+#>>> []
+
+print remove_tags("This is plain text.")
+#>>> ['This', 'is', 'plain', 'text.']
